4c6b44086e94e0c6589a3efb136cda73a226e2e8
Mohamud <msheeq99@outlook.com>
2023-01-25 20:33:43 +0000

    db seeds and schema added

---
+++ db/seeds.sql
@@ -0,0 +1,27 @@
+use employees_db;
+
+INSERT INTO department (name)
+VALUES ("Legal"),
+       ("HR"),
+       ("Engineering"),
+       ("Sales"),
+       ("Accounting");
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Sales person", 50000, 4),
+       ("Sales manger", 60000, 4),
+       ("Engineer", 440000, 3),
+       ("HR REP", 320000, 2),
+       ("Accountant", 50000, 5),
+       ("Company Attorney", 70000, 1),
+       ("Engineering Manager", 70000, 3);
+       
+
+ INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Mark", "Allen", 2, NULL),
+       ("John", "Smith", 1, 1),
+       ("Adam", "Peters", 7, NULL),
+       ("Sam", "Hill", 3, 3),
+       ("Will", "Atkins", 4, 1),
+       ("Jack", "Good", 5, 1),
+       ("Lisa", "Holt", 6, 1);
