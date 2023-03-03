348e68fa4006c2e6fe2362bbac65c48c0474144a
Mohamud <msheeq99@outlook.com>
2023-03-03 22:50:01 +0000

    switch case addEmployee and addDepartment completed

--- server.js
+++ server.js
@@ -149,3 +149,92 @@ const badCompany = () => {
       }
     });
 };
+
+// ALL FX PER SWITCH CASE
+const addDepartment = () => {
+  // show the current Departments in the database
+  const query = "SELECT * FROM department";
+  connection.query(query, (err, results) => {
+    if (err) throw err;
+
+    console.log(chalk.blue("List of current departments"));
+
+    console.table(results);
+
+    // ask what the name is for the new dept
+    inquirer
+      .prompt([
+        {
+          name: "newDept",
+          type: "input",
+          message: "What department would you like to add?",
+        },
+      ])
+      .then((answer) => {
+        connection.query(
+          `INSERT INTO department(name) VALUES(?)`,
+          [answer.newDept],
+          (err, results) => {
+            badCompany();
+          }
+        );
+      });
+  });
+};
+
+const addEmployee = () => {
+  connection.query(roleQuery, (err, results) => {
+    if (err) throw err;
+
+    inquirer
+      .prompt([
+        {
+          name: "fName",
+          type: "input",
+          message: askNewEmployee[0],
+        },
+
+        {
+          name: "lName",
+          type: "input",
+          message: askNewEmployee[1],
+        },
+
+        {
+          name: "role",
+          type: "list",
+          // A FX in the choices creates a new array from results (all from roles table) loops and returns
+          // the array of titles
+          choices: function () {
+            let choiceArr = results[0].map((choice) => choice.title);
+            return choiceArr;
+          },
+          //   asking what the role is?
+          message: askNewEmployee[2],
+        },
+        {
+          name: "manager",
+          type: "list",
+          // A FX that creates a new array from employee table, the concatenated first and last name
+          // and returns an array of the full name
+          choices: function () {
+            let choiceArr = results[1].map((choice) => choice.full_name);
+            return choiceArr;
+          },
+          // asking who is their manager
+          message: askNewEmployee[3],
+        },
+      ])
+      .then((answer) => {
+        connection.query(
+          `INSERT INTO employee(first_name,last_name, role_id, manager_id) 
+          VALUES (?,?, 
+            (SELECT id FROM roles WHERE title = ?), 
+            (SELECT id FROM (SELECT id FROM employee WHERE CONCAT(first_name,'',last_name) = ?)
+            AS tmptable))`,
+          [answer.fName, answer.lName, answer.role, answer.manager]
+        );
+        badCompany();
+      });
+  });
+};
